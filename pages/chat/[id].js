9d2a922937afb6d97d00c6d92400e5d0cfb760dd
KaranP3 <karanpillai93@gmail.com>
2022-07-11 20:02:03 +0530

    Remove auth related code from [id].js

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -9,12 +9,13 @@ import {
 } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar";
 import Head from "next/head";
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 const Topbar = () => {
   return (
     <Flex bg="gray.100" h="81px" w="100%" align="center" p="5">
       <Avatar src="" marginEnd="3" />
-      <Heading size="md">{user.displayName}</Heading>
+      <Heading size="md">user@gmail.com</Heading>
     </Flex>
   );
 };
